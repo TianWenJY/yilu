2777a7f352f8725bfd8cb858bb0e85e80b8027cf
unknown <359115212@qq.com>
2017-09-19 17:13:20 +0800

    wb第二次提交

---
+++ js/login.js
@@ -0,0 +1,21 @@
+/**
+ * Created by will on 2017/9/19.
+ */
+
+//密码强度正则，最少6位，包括至少1个大写字母，1个小写字母，1个数字，1个特殊字符
+var pPattern = /^.*(?=.{6,})(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%^&*? ]).*$/;
+function test(){
+    var text="index.aspx?test=1&ww=2&www=3"; //
+    //用户名正则，4到16位（字母，数字，下划线，减号）
+    var uPattern = /^[a-zA-Z0-9_-]{4,16}$/;
+    //  var   re =new RegExp("\\?(\\w{1,}=\\w{1,}&){1,}\\w{1,}=\\w{1,}");
+    var result=  re.test(text);
+    if(result)
+    {
+        alert("ok");
+    }else
+    {
+        alert("err");
+    }
+
+}
